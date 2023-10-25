b0ff358ace8b434d491f3ed0fa3cf10b520b0c50
Raf Cami <r0463108@student.thomasmore.be>
2023-10-25 11:26:56 +0200

    fix env

--- src/api/MoviesAPI.tsx
+++ src/api/MoviesAPI.tsx
@@ -3,14 +3,14 @@ import { UseQueryResult, useQuery } from '@tanstack/react-query'
 import IMovies from '../models/IMovies'
 
 const client = axios.create({
-    baseURL: 'https://api.themoviedb.org/3/movie/',
-  })
-
-  const getMovies = async (): Promise<IMovies[]> => {
-    // https://api.themoviedb.org/3/movie/popular?api_key=
-    const { data } = await client.get(`popular?api_key=import.meta.env.VITE_MOVIEDB_API_KEY`)
-    // await new Promise(resolve => setTimeout(resolve, 5000))
-    return data.results
+        baseURL: 'https://api.themoviedb.org/3/movie/',
+    })
+    
+    const getMovies = async (): Promise<IMovies[]> => {
+        // https://api.themoviedb.org/3/movie/popular?api_key=
+        const { data } = await client.get(`popular`, { params: { api_key: import.meta.env.VITE_MOVIEDB_API_KEY } })
+        // await new Promise(resolve => setTimeout(resolve, 5000))
+        return data.results
 }
 
 export const useGetMovies = (): UseQueryResult<IMovies[], Error> => { 

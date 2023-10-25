2d745dc17621b39b24aa06052d2d9b19dce29651
Raf Cami <r0463108@student.thomasmore.be>
2023-10-25 10:57:31 +0200

    done

--- src/api/MoviesAPI.tsx
+++ src/api/MoviesAPI.tsx
@@ -2,13 +2,12 @@ import axios from 'axios'
 import { UseQueryResult, useQuery } from '@tanstack/react-query'
 import IMovies from '../models/IMovies'
 
-
 const client = axios.create({
     baseURL: 'https://api.themoviedb.org/3/movie/',
   })
 
   const getMovies = async (): Promise<IMovies[]> => {
-    // https://api.themoviedb.org/3/movie/popular?api_key=import.meta.env.VITE_MOVIEDB_API_KEY
+    // https://api.themoviedb.org/3/movie/popular?api_key=
     const { data } = await client.get(`popular?api_key=import.meta.env.VITE_MOVIEDB_API_KEY`)
     // await new Promise(resolve => setTimeout(resolve, 5000))
     return data.results
